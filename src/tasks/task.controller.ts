e443d959ad35a380e8a15ac01be3c57a57685a47
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-09-29 18:33:34 +0500

    Put, Patch and Delete routes

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import type { UUID } from 'crypto';
 import { TaskService } from './task.service';
 import type { TaskModel } from './task.model';
+
 import { CreateTaskDto } from './dto/create-task.dto';
-import type { UUID } from 'crypto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
 export class TaskController {
@@ -14,12 +25,27 @@ export class TaskController {
   }
 
   @Get(':id')
-  getTaskById(@Param('id') id: UUID) {
+  getTaskById(@Param('id') id: UUID): TaskModel {
     return this.taskService.getTaskById(id);
   }
 
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): TaskModel {
     return this.taskService.createTask(createTaskDto);
+  }
+
+  @Put(':id')
+  updateTask(@Param('id') id: UUID, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.taskService.updateTask(id, updateTaskDto);
+  }
+
+  @Patch(':id')
+  semiUpdateTask(@Param(':id') id: UUID, updateTaskDto: UpdateTaskDto) {
+    return this.taskService.semiUpdateTask(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: UUID): TaskModel {
+    return this.taskService.deleteTask(id);
   }
 }
--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -3,6 +3,7 @@ import { TaskModel, TaskStatus } from './task.model';
 import { randomUUID, type UUID } from 'crypto';
 
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -29,5 +30,47 @@ export class TaskService {
     };
     this.tasks.push(task);
     return task;
+  }
+
+  updateTask(id: UUID, updateTaskDto: UpdateTaskDto): TaskModel {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+    if (taskIndex === -1) throw new NotFoundException();
+
+    const { title, description, status } = updateTaskDto;
+
+    this.tasks[taskIndex] = {
+      id,
+      title: title as string,
+      description: description as string,
+      status: status as TaskStatus,
+    };
+
+    return this.tasks[taskIndex];
+  }
+
+  semiUpdateTask(id: UUID, updateTaskDto: UpdateTaskDto): TaskModel {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+    if (taskIndex === -1) throw new NotFoundException();
+
+    if (updateTaskDto.title !== undefined) {
+      this.tasks[taskIndex].title = updateTaskDto.title;
+    }
+    if (updateTaskDto.description !== undefined) {
+      this.tasks[taskIndex].description = updateTaskDto.description;
+    }
+    if (updateTaskDto.status !== undefined) {
+      this.tasks[taskIndex].status = updateTaskDto.status;
+    }
+
+    return this.tasks[taskIndex];
+  }
+
+  deleteTask(id: UUID): TaskModel {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+    if (taskIndex === -1) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    const [deletedTask] = this.tasks.splice(taskIndex, 1);
+    return deletedTask;
   }
 }
